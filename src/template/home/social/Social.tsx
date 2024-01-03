5216e434a392c88500556a2562d789de406b8372
leonardo.goncalves <leogoncalves.contato@gmail.com>
2024-01-03 14:55:21 -0300

    ⚰️ remove unused templates

--- src/template/home/social/Social.tsx
+++ src/template/home/social/Social.tsx
@@ -1,75 +1,84 @@
+import { useRef } from 'react';
+
 import Box from '@mui/material/Box';
 import Fade from '@mui/material/Fade';
 import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
+import Chip from '@mui/material/Chip';
+import Zoom from '@mui/material/Zoom';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
 
+import Title from '@/components/Title';
+import { CLICK_EVENT } from '@/events/events';
+import { tracking, url } from '@/services/core';
+import useIsInViewport from '@/hooks/useIsInViewport';
+import Trigger from '@/components/Trigger';
+
 import config, { type Config } from './config';
 
-function Title() {
-    return (
-        <Box textAlign="center">
-            <Typography
-                variant="button"
-                gutterBottom
-                component="p"
-                color="text.secondary">
-                Integrações
-            </Typography>
-            <Typography
-                variant="h4"
-                gutterBottom
-                component="h4"
-                color="text.primary"
-                fontFamily="Inter, sans-serif"
-            >
-                Redes sociais integradas
-            </Typography>
-            <Typography
-                variant="h6"
-                gutterBottom
-                component="h6"
-                color="text.secondary"
-                fontFamily="Inter, sans-serif"
-            >
-                Transforme a sua presença online com a automação de redes sociais que vai além dos limites
-            </Typography>
-        </Box>
-    );
-}
-
-function GridSocials() {
+interface GridSocialsProps { show: boolean; }
+function GridSocials({ show }: GridSocialsProps) {
     return (
         <Grid container spacing={3}>
             {
                 config.map((social, index) => (
-                    <Grid key={social.title} item xs={12} sm={6} md={6}>
-                        <SocialCard
-                            icon={social.icon}
-                            title={social.title}
-                            label={social.label}
-                            disabled={social.disabled}
-                            description={social.description}
-                        />
-                    </Grid>
+                    <Zoom
+                        key={index}
+                        in={show}
+                        style={{
+                            transitionDelay: `${index * 200}ms`,
+                        }}
+                    >
+                        <Grid item xs={12} sm={6} md={6}>
+                            <SocialCard
+                                tag={social.tag}
+                                icon={social.icon}
+                                title={social.title}
+                                label={social.label}
+                                disabled={social.disabled}
+                                description={social.description}
+                            />
+                        </Grid>
+                    </Zoom>
+
                 ))
             }
         </Grid>
     );
 }
 
-function SocialCard({ title, description, icon, label, disabled }: Config) {
+function SocialCard({ title, description, icon, label, disabled, tag }: Config) {
+    const getStart = () => {
+        tracking.event(CLICK_EVENT('create_workflow_click'));
+        window.location.href = `${url.manager}/workflow?create-workflow=true`;
+    };
+
     return (
         <Card elevation={0}>
             <CardContent>
                 <Stack spacing={1} useFlexGap>
-                    <Box>
-                        {icon}
+                    <Box display="flex" justifyContent="space-between" alignItems="center" sx={{ mb: 2 }}>
+                        <Box>
+                            {icon}
+                        </Box>
+                        {
+                            tag && <Box>
+                                <Chip
+                                    color="secondary"
+                                    label={tag}
+                                    variant="outlined"
+                                    sx={{
+                                        fontFamily: 'Inter, sans-serif',
+                                    }}
+                                />
+                            </Box>
+                        }
                     </Box>
+
                     <Box>
                         <Typography variant="h4" component="h4" gutterBottom>
                             {title}
@@ -83,6 +92,7 @@ function SocialCard({ title, description, icon, label, disabled }: Config) {
                         variant="contained"
                         disabled={disabled}
                         sx={{ mt: 2 }}
+                        onClick={getStart}
                     >
                         {label}
                     </Button>
@@ -93,12 +103,28 @@ function SocialCard({ title, description, icon, label, disabled }: Config) {
 }
 
 export default function Social() {
+    const titleRef = useRef<HTMLDivElement>(null);
+    const [, titleDisplayed] = useIsInViewport(titleRef);
+
+    const gridRef = useRef<HTMLDivElement>(null);
+    const [, gridDisplayed] = useIsInViewport(titleRef);
+
     return (
-        <Fade in>
+        <Fade in={titleDisplayed}>
             <Container sx={{ p: 5 }}>
-                <Title />
-                <Box my={5}>
-                    <GridSocials />
+                <Fade in={titleDisplayed}>
+                    <div style={{ position: 'relative' }}>
+                        <Title
+                            section="Integrações"
+                            title="Redes sociais integradas"
+                            subtitle="Transforme a sua presença online com a automação de redes sociais que vai além dos limites"
+                        />
+                        <Trigger ref={titleRef} />
+                    </div>
+                </Fade>
+                <Box my={5} position="relative">
+                    <GridSocials show={gridDisplayed} />
+                    <Trigger ref={gridRef} />
                 </Box>
             </Container>
         </Fade>
--- src/template/home/social/config.tsx
+++ src/template/home/social/config.tsx
@@ -1,26 +1,22 @@
-// import Whatsapp from './Whatsapp';
-// import Telegram from './Telegram';
-// import Instagram from './Instagram';
-// import Messenger from './Messenger';
-
 import Telegram from '@mui/icons-material/Telegram';
 import Instagram from '@mui/icons-material/Instagram';
 import Facebook from '@mui/icons-material/Facebook';
 import WhatsApp from '@mui/icons-material/WhatsApp';
 
 export interface Config {
+    tag?: string;
+    label: string;
     title: string;
+    disabled: boolean;
     description: string;
     icon: React.JSX.Element;
-    disabled: boolean;
-    label: string;
 }
 
 export default [
     {
+        tag: 'Novo!',
         title: 'Instagram',
         description: 'Automatização de processos de vendas no Instagram não é apenas uma tendência, mas uma necessidade para empresas que desejam permanecer competitivas no mercado digital',
-        // icon: <Instagram size={2.5} sx={{ fontSize: 40, color: '#DD2A7B' }} />,
         icon: <Instagram sx={{ fontSize: 40, color: '#DD2A7B' }} />,
         label: 'Começar',
         disabled: false
@@ -28,7 +24,6 @@ export default [
     {
         title: 'Whatsapp',
         description: 'WhatsApp emerge como uma ferramenta estratégica e indispensável para as empresas. Facilita a comunicação instantânea, personalização de interações e acompanhamento eficiente',
-        // icon: <Whatsapp size={2.5} sx={{ fontSize: 40, color: '#DD2A7B' }} />,
         icon: <WhatsApp sx={{ fontSize: 40, color: '#075e54' }} />,
         label: 'Em breve',
         disabled: true
@@ -36,7 +31,6 @@ export default [
     {
         title: 'Telegram',
         description: 'Telegram se destaca como uma ferramenta estratégica para empresas que buscam aprimorar suas estratégias digitais é um meio poderoso para impulsionar o desempenho comercial',
-        // icon: <Telegram size={2.5} sx={{ fontSize: 40, color: '#DD2A7B' }} />,
         icon: <Telegram sx={{ fontSize: 40, color: '#0088CC' }} />,
         label: 'Em breve',
         disabled: true
@@ -44,7 +38,6 @@ export default [
     {
         title: 'Messenger',
         description: 'Messenger vai além de uma simples eficiência; é uma abordagem estratégica para empresas que buscam se destacar em um mercado dinâmico',
-        // icon: <Messenger size={2.5} sx={{ fontSize: 40, color: '#DD2A7B' }} />,
         icon: <Facebook sx={{ fontSize: 40, color: '#3B5998' }} />,
         label: 'Em breve',
         disabled: true
